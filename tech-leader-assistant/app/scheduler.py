365d4952a26f1d9b38e8c25b74366de0dcae5de3
pimenovdv <pimenov.d.v@yandex.ru>
2026-07-24 06:52:06 +0300

    feat: implement mr jira validator task and tests (#36)
    Co-authored-by: pimenovdv <25670278+pimenovdv@users.noreply.github.com>

--- tech-leader-assistant/app/scheduler.py
+++ tech-leader-assistant/app/scheduler.py
@@ -9,7 +9,8 @@ from .tasks import (
     stale_jira_task_reminder_task,
     gitlab_mr_size_labeler_task,
     gitlab_draft_labeler_task,
-    gitlab_merged_branch_cleanup_task
+    gitlab_merged_branch_cleanup_task,
+    gitlab_mr_jira_validator_task
 )
 import logging
 
@@ -43,6 +44,8 @@ def setup_scheduler():
     scheduler.add_job(gitlab_draft_labeler_task, 'cron', hour='*', minute=30, id="gitlab_draft_labeler")
 
     scheduler.add_job(gitlab_merged_branch_cleanup_task, 'cron', hour=5, minute=0, id="gitlab_merged_branch_cleanup")
+
+    scheduler.add_job(gitlab_mr_jira_validator_task, 'cron', hour='*', minute=45, id="gitlab_mr_jira_validator")
 
     logger.info("APScheduler jobs configured.")
 
